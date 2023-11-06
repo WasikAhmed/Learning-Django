dca2e78964928198b82577a6a64c17361d43c140
Wasik Ahmed Apon <22-47698-2@student.aiub.edu>
2023-11-07 02:59:47 +0600

    Update todo_list: Add custom form for creating items

--- todo_list/main/views.py
+++ todo_list/main/views.py
@@ -7,6 +7,28 @@ from main.forms import CreateNewList
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
+
+    if response.method == "POST":
+        print(response.POST)
+        if response.POST.get("save"):
+            for item in ls.item_set.all():
+                if response.POST.get("c" + str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+            
+                item.save()
+
+
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("new")
+
+            if len(txt) > 2:
+                ls.item_set.create(text=txt, complete=False)
+            else:
+                print("invalid")
+
+
     # item = ls.item_set.get(id=1)
     # return HttpResponse("<h1>%s</h1></br><p>%s</p>" %(ls.name, str(item.text)))
     return render(response, "main/list.html", {"ls":ls})
