cd86753b301ae1e6e47eaee6e5b1307d0cc843e7
Wasik Ahmed Apon <22-47698-2@student.aiub.edu>
2023-11-07 02:26:18 +0600

    Update todo_list: Add simple form for creating a todo list (#1)

--- todo_list/main/views.py
+++ todo_list/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from main.models import ToDoList, Item
+from main.forms import CreateNewList
 
 # Create your views here.
 
@@ -10,9 +11,20 @@ def index(response, id):
     # return HttpResponse("<h1>%s</h1></br><p>%s</p>" %(ls.name, str(item.text)))
     return render(response, "main/list.html", {"ls":ls})
 
-
 def home(response):
-    return render(response, "main/home.html", {"name":"test"})
+    return render(response, "main/home.html", {"na me":"test"})
 
-def hello(response, id):
-    return HttpResponse("Hello")
+def create(response):
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = ToDoList(name=n)
+            t.save()
+            
+        return HttpResponseRedirect("/%i" %t.id)
+    else:
+        form = CreateNewList()
+    
+    return render(response, "main/create.html", {"form":form})
