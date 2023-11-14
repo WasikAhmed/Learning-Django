40e635481f39017d47eb3668317912a6aadc9d34
Wasik Ahmed Apon <22-47698-2@student.aiub.edu>
2023-11-14 12:41:56 +0600

    Fix virtual environment issues

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/wasik/GitHub/LearningDjango/venv"
+set -gx VIRTUAL_ENV "/Users/wasik/GitHub/Learning-Django/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
