fa53686c50bf93ad5d40c643e87174b55bf89892
Wasik Ahmed Apon <22-47698-2@student.aiub.edu>
2023-11-30 08:16:38 +0600

    Remove virtual environment Add: gitignore

--- QuickNotes/main/views.py
+++ QuickNotes/main/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 def index(response):
-    return HttpResponse("<h1>Hello, World</h1>")
+    return render(response, "main/index.html", {})
+
+
+def create(response):
+    return render(response, "main/create.html", {})
+
